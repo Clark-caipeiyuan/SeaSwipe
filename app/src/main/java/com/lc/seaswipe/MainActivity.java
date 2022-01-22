bfff4151d809fa57a81a5d293441f029c8bd72ea
Clark <caicai__1377@163.com>
2022-01-22 10:48:40 +0800

    版本编译报错问题-去掉kotlin依赖

---
+++ app/src/main/java/com/lc/seaswipe/MainActivity.java
@@ -0,0 +1,30 @@
+/**
+ * FileName: MainActivity
+ * Author: clark
+ * Date: 2022/1/22 10:37 上午
+ * Description:
+ * History:
+ * <author> <time> <version> <desc>
+ * 作者姓名 修改时间 版本号 描述
+ */
+package com.lc.seaswipe;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * @ClassName: MainActivity
+ * @Description: java类作用描述
+ * @Author: clark
+ * @Date: 2022/1/22 10:37 上午
+ */
+public class MainActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
